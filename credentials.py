2c5ce93c865984bde6f2b77de43f42df5ee20dcb
zlhartzog <zlhartzog@crimson.ua.edu>
2017-11-27 11:19:10 -0600

    Initial Push + Setup
    Setup of code

---
+++ credentials.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "ZWXt08DEdnhVI03qiuPsKPZHh"
+consumer_secret = "pQ6ig3GIDUGoWKDsp3iZ3Eni7oeFJUicZQ0dlcc9GK8rDGBWL0"
+access_key = "3991702223-3cfdE4qANGabFTfmjVrD5yBg1eoI8pDiDP6ro2p"
+access_secret = "NYxtKqDdxCF72Q8FRIPMg21KJOb5X7nQy6acu1825UyUT"
---
+++ game.py
@@ -0,0 +1,161 @@
+# General:
+import tweepy           # To consume Twitter's API
+import pandas as pd     # To handle data
+import numpy as np      # For number computing
+
+# For plotting and visualization:
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline
+
+from textblob import TextBlob
+import re
+
+# We import our access keys:
+from credentials import *    # This will allow us to use the keys as variables
+
+# API's setup:
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with our access keys provided.
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+#CHANGE THIS
+#=================================================================
+# We create an extractor object:
+extractor = twitter_setup()
+
+# We create a tweet list as follows:
+tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
+print("Number of tweets extracted: {}.\n".format(len(tweets)))
+
+# We print the most recent 5 tweets:
+print("5 recent tweets:\n")
+for tweet in tweets[:5]:
+    print(tweet.text)
+    print()
+#=================================================================
+
+# We create a pandas dataframe as follows:
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+# We display the first 10 elements of the dataframe:
+display(data.head(10))
+
+# We add relevant data:
+data['len']  = np.array([len(tweet.text) for tweet in tweets])
+data['ID']   = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+
+# Display of first 10 elements from dataframe:
+display(data.head(10))
+
+# We extract the mean of lenghts:
+mean = np.mean(data['len'])
+print("The average length: {}".format(mean))
+
+# We extract the tweet with more FAVs and more RTs:
+
+fav_max = np.max(data['Likes'])
+rt_max  = np.max(data['RTs'])
+
+fav = data[data.Likes == fav_max].index[0]
+rt  = data[data.RTs == rt_max].index[0]
+
+# Max FAVs:
+print("The tweet with more likes is: \n{}".format(data['Tweets'][fav].encode('utf-8')))
+print("Number of likes: {}".format(fav_max))
+print("{} characters.\n".format(data['len'][fav]))
+
+# Max RTs:
+print("The tweet with more retweets is: \n{}".format(data['Tweets'][rt].encode('utf-8')))
+print("Number of retweets: {}".format(rt_max))
+print("{} characters.\n".format(data['len'][rt]))
+
+# We create time series for data:
+
+tlen = pd.Series(data=data['len'].values, index=data['Date'])
+tfav = pd.Series(data=data['Likes'].values, index=data['Date'])
+tret = pd.Series(data=data['RTs'].values, index=data['Date'])
+
+# Lenghts along time:
+tlen.plot(figsize=(16,4), color='r');
+
+# Likes vs retweets visualization:
+tfav.plot(figsize=(16,4), label="Likes", legend=True)
+tret.plot(figsize=(16,4), label="Retweets", legend=True);
+
+# We obtain all possible sources:
+sources = []
+for source in data['Source']:
+    if source not in sources:
+        sources.append(source)
+
+# We print sources list:
+print("Creation of content sources:")
+for source in sources:
+    print("* {}".format(source))
+
+# We create a numpy vector mapped to labels:
+percent = np.zeros(len(sources))
+
+for source in data['Source']:
+    for index in range(len(sources)):
+        if source == sources[index]:
+            percent[index] += 1
+            pass
+
+percent /= 100
+
+# Pie chart:
+pie_chart = pd.Series(percent, index=sources, name='Sources')
+pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6));
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+
+# We create a column with the result of the analysis:
+data['SA'] = np.array([ analize_sentiment(tweet) for tweet in data['Tweets'] ])
+
+# We display the updated dataframe with the new column:
+display(data.head(20))
+
+# We construct lists with classified tweets:
+
+pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] > 0]
+neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == 0]
+neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] < 0]
+
+# We print percentages:
+print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['Tweets'])))
+print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['Tweets'])))
+print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data['Tweets'])))
