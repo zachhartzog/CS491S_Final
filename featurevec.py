3882d26ddcb4f4c0d1279e07c9427c3543403aea
zlhartzog <zlhartzog@crimson.ua.edu>
2017-12-01 19:59:38 -0600

    December 1, 2017

---
+++ featurevec.py
@@ -0,0 +1,93 @@
+import re
+
+###Preprocess tweets
+def processTweet2(tweet):
+    # process the tweets
+
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+','AT_USER',tweet)
+    #Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    #trim
+    tweet = tweet.strip('\'"')
+    return tweet
+
+
+###get stopword list
+def getStopWordList(stopWordListFileName):
+    #read the stopwords file and build a list
+    stopWords = []
+    stopWords.append('AT_USER')
+    stopWords.append('URL')
+
+    fp = open(stopWordListFileName, 'r')
+    line = fp.readline()
+    while line:
+        word = line.strip()
+        stopWords.append(word)
+        line = fp.readline()
+    fp.close()
+    return stopWords
+
+stopWords = []
+
+st = open('stopwords.txt', 'r')
+stopWords = getStopWordList('stopwords.txt')
+
+
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+#end
+
+def getFeatureVector(tweet):
+    featureVector = []
+    #split tweet into words
+    words = tweet.split()
+    for w in words:
+        #replace two or more with two occurrences
+        w = replaceTwoOrMore(w)
+        #strip punctuation
+        w = w.strip('\'"?,.')
+        #check if the word stats with an alphabet
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        #ignore if it is a stop word
+        if(w in stopWords or val is None):
+            continue
+        else:
+            featureVector.append(w.lower())
+    return featureVector
+
+###load airline sentiment training data
+
+#airlinetrain = pd.read_csv("Airline-Sentiment-2-w-AA.csv", encoding ="ISO-8859-1")
+#tweets = []
+
+#featureList = []
+#for i in range(len(airlinetrain)):
+def featureGrab(tweet):
+    #sentiment = airlinetrain['airline_sentiment'][i]
+    #tweet = airlinetrain['text'][i]
+    processedTweet = processTweet2(tweet)
+    featureVector = getFeatureVector(processedTweet)
+    return featureVector
+    #featureList.extend(featureVector)
+    #tweets.append((featureVector, sentiment))
+
+def extract_features(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in featureList:
+        features['contains(%s)' % word] = (word in tweet_words)
+    return features
+#end
+
+### Remove featureList duplicates
+#featureList = list(set(featureList))
--- game.py
+++ game.py
@@ -1,20 +1,22 @@
 # General:
-import tweepy           # To consume Twitter's API
+#import tweepy           # To consume Twitter's API
 import pandas as pd     # To handle data
 import numpy as np      # For number computing
 
-# For plotting and visualization:
 from IPython.display import display
-import matplotlib.pyplot as plt
-import seaborn as sns
-#%matplotlib inline
-
-from textblob import TextBlob
 import re
+import csv
+
+import nltk
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # We import our access keys:
 from credentials import *    # This will allow us to use the keys as variables
 
+'''
 # API's setup:
 def twitter_setup():
     """
@@ -28,132 +30,66 @@ def twitter_setup():
     # Return API with authentication:
     api = tweepy.API(auth)
     return api
-
+'''
 #CHANGE THIS
 #=================================================================
 # We create an extractor object:
+'''
 extractor = twitter_setup()
 
 # We create a tweet list as follows:
-tweets = extractor.user_timeline(screen_name="realDonaldTrump", count=200)
+tweets = extractor.search(q="Alabama Football", lang="en", count=20)
 print("Number of tweets extracted: {}.\n".format(len(tweets)))
 
 # We print the most recent 5 tweets:
-print("5 recent tweets:\n")
-for tweet in tweets[:5]:
+print("recent tweets:\n")
+for tweet in tweets:
     print(tweet.text)
     print()
+'''
 #=================================================================
 
-# We create a pandas dataframe as follows:
-data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+with open('2015_lsu_alabama.csv') as myFile:
+    reader = csv.reader(myFile)
+    data = pd.DataFrame(data=[row[0] for row in reader], columns=['Tweets'])
 
-# We display the first 10 elements of the dataframe:
-display(data.head(10))
-
-# We add relevant data:
-data['len']  = np.array([len(tweet.text) for tweet in tweets])
-data['ID']   = np.array([tweet.id for tweet in tweets])
-data['Date'] = np.array([tweet.created_at for tweet in tweets])
-data['Source'] = np.array([tweet.source for tweet in tweets])
-data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
-data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
-
-# Display of first 10 elements from dataframe:
-display(data.head(10))
-
-# We extract the mean of lenghts:
-mean = np.mean(data['len'])
-print("The average length: {}".format(mean))
-
-# We extract the tweet with more FAVs and more RTs:
-
-fav_max = np.max(data['Likes'])
-rt_max  = np.max(data['RTs'])
-
-fav = data[data.Likes == fav_max].index[0]
-rt  = data[data.RTs == rt_max].index[0]
-
-# Max FAVs:
-print("The tweet with more likes is: \n{}".format(data['Tweets'][fav].encode('utf-8')))
-print("Number of likes: {}".format(fav_max))
-print("{} characters.\n".format(data['len'][fav]))
-
-# Max RTs:
-print("The tweet with more retweets is: \n{}".format(data['Tweets'][rt].encode('utf-8')))
-print("Number of retweets: {}".format(rt_max))
-print("{} characters.\n".format(data['len'][rt]))
-
-# We create time series for data:
-
-tlen = pd.Series(data=data['len'].values, index=data['Date'])
-tfav = pd.Series(data=data['Likes'].values, index=data['Date'])
-tret = pd.Series(data=data['RTs'].values, index=data['Date'])
-
-# Lenghts along time:
-tlen.plot(figsize=(16,4), color='r');
-
-# Likes vs retweets visualization:
-tfav.plot(figsize=(16,4), label="Likes", legend=True)
-tret.plot(figsize=(16,4), label="Retweets", legend=True);
-
-# We obtain all possible sources:
-sources = []
-for source in data['Source']:
-    if source not in sources:
-        sources.append(source)
-
-# We print sources list:
-print("Creation of content sources:")
-for source in sources:
-    print("* {}".format(source))
-
-# We create a numpy vector mapped to labels:
-percent = np.zeros(len(sources))
-
-for source in data['Source']:
-    for index in range(len(sources)):
-        if source == sources[index]:
-            percent[index] += 1
-            pass
-
-percent /= 100
-
-# Pie chart:
-pie_chart = pd.Series(percent, index=sources, name='Sources')
-pie_chart.plot.pie(fontsize=11, autopct='%.2f', figsize=(6, 6));
-
-def clean_tweet(tweet):
-    '''
-    Utility function to clean the text in a tweet by removing
-    links and special characters using regex.
-    '''
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
-
-def analize_sentiment(tweet):
-    '''
-    Utility function to classify the polarity of a tweet
-    using textblob.
-    '''
-    analysis = TextBlob(clean_tweet(tweet))
-    if analysis.sentiment.polarity > 0:
-        return 1
-    elif analysis.sentiment.polarity == 0:
-        return 0
-    else:
-        return -1
-
-# We create a column with the result of the analysis:
-data['SA'] = np.array([ analize_sentiment(tweet) for tweet in data['Tweets'] ])
-
-# We display the updated dataframe with the new column:
 display(data.head(20))
 
-# We construct lists with classified tweets:
+print "Getting Analysis..."
+sid = SentimentIntensityAnalyzer()
+for tweet in data['Tweets']:
+   print(tweet)
 
-pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] > 0]
-neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == 0]
-neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] < 0]
+test_data = "Manchurian was hot and spicy"
+test_data_features = {word.lower(): (word in word_tokenize(test_data.lower())) for word in dictionary}
+
+print (classifier.classify(test_data_features))
+
+
+data['NLTK'] = np.array([ max(sid.polarity_scores(tweet), key=lambda x: x[1]) for tweet in data['Tweets'] ])
+
+display(data.head(30))
+'''
+with open('2015_lsu_alabamaDATUM.csv', 'w') as csvfile:
+    fieldnames = ['text', 'analysis']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    i = 0
+    for tweet in data['Tweets']:
+        print i + "\t" + tweet + "\n"
+        writer.writerow({'text': tweet, 'analysis': datum_box.twitter_sentiment_analysis(tweet).encode('utf-8')})
+        i++
+'''
+#for i in range(30):
+    #print datum_box.twitter_sentiment_analysis(data['Tweets'][i]).encode('utf-8')
+#data['DATUM'] = np.array([ datum_box.twitter_sentiment_analysis(tweet).encode('utf-8') for tweet in data['Tweets'] ])
+
+# We display the updated dataframe with the new column:
+display(data.head(30))
+
+pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['NLTK'][index] == 'pos']
+neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['NLTK'][index] == 'neu']
+neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['NLTK'][index] == 'neg']
 
 # We print percentages:
 print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['Tweets'])))
